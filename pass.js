d57416ba4796406ac75a39ce90e3fdf7cfae43ef
pewing <philip.jh.ewing@gmail.com>
2016-07-11 19:10:13 -0700

    changes to barry white

--- pass.js
+++ pass.js
@@ -1,23 +1,42 @@
-$( document ).ready(function() {
-  console.log (" DOCUMENT LOADED");
+function setBarry(){
+  chrome.storage.local.get(["barry_status"], function (items) {
+    var barry_status = items["barry_status"];
+    console.log(barry_status);
+    if ( barry_status == "barry") {
+      chrome.storage.local.set({"barry_status": "no_barry"});
+      console.log('unbarried');
+    }
+    else {
+      chrome.storage.local.set({"barry_status": "barry"});
+      console.log('barried');
+    }
+  });
 
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('barry-button').addEventListener('click', setBarry);
+});
+
+
+$( document ).ready(function() {
   var rick_roll_urls = ['https://www.youtube.com/watch?v=dQw4w9WgXcQ',
                         'http://rickrollomatic.com/'];
 
   $('a').each( function(i) {
-
-    // var query_string = 'http://www.youtube.com/oembed?url=' +
-    //   $(this).attr('href')+'&format=json';
-    // console.log(query_string);
-
-    // var query_result = $.getJSON(query_string);
-
-    if ($.inArray($(this).attr('href'), rick_roll_urls) !== -1) {
-      console.log("RICKROLLED", $(this).attr('href'));
+    var that = $(this);
+    if ($.inArray(that.attr('href'), rick_roll_urls) !== -1) {
+      chrome.storage.local.get(["barry_status"], function (items) {
+        if (items["barry_status"] == "barry") {
+          that.attr('href', 'https://www.youtube.com/watch?v=eLdSYF0WxyE')
+          console.log("BARRIED");
+        }
+        else {
+          that.text("RICK ROLL");
+          that.css("color","red");
+        }
+      });
     }
-    // else {
-    //   console.log("NOT RICKROLLED", $(this).attr('href'));
-    // }
   });
 });
 
