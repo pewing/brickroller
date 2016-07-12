4c6b27f9b88d2e0de9be3aeef543637e5363d568
pewing <philip.jh.ewing@gmail.com>
2016-07-11 20:13:31 -0700

    consolidate setBarry code

---
+++ context.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+
+  function reloadPage() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+      var code = 'window.location.reload();';
+      chrome.tabs.executeScript(arrayOfTabs[0].id, {code: code});
+    });
+  }
+
+  function setBarry(status) {
+    chrome.storage.local.set({"barry_status": status}, function () {
+      chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
+        var code = 'window.location.reload();';
+        chrome.tabs.executeScript(arrayOfTabs[0].id, {code: code});
+      });
+      location.reload();
+    });
+  }
+
+  function updateBarry() {
+    chrome.storage.local.get(["barry_status"], function (items) {
+      if ( items["barry_status"] == "barry") {
+        setBarry("no_barry");
+      }
+      else {
+        setBarry("barry");
+      }
+    });
+  }
+
+  $('#barry-button').on('click', updateBarry);
+
+  chrome.storage.local.get(["barry_status"], function (items) {
+    if ( items["barry_status"] == "barry") {
+      console.log("supposed to check now");
+      $('#barry-button').prop('checked', true);
+    }
+    else {
+      $('#barry-form').prop('checked', false);
+    }
+  });
+
+});
